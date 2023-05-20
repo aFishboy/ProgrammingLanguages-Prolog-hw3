668cb795b389857e8601bfa245542263f86059e3
aFishboy <samuelhoward914@gmail.com>
2023-05-20 01:12:52 -0700

    query done

--- query/query.pl
+++ query/query.pl
@@ -1,29 +1,37 @@
 /* All novels published during the year 1953 or 1996*/
 year_1953_1996_novels(Book) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    novel(Book, 1953); 
+    novel(Book, 1996).
 
 /* List of all novels published during the period 1800 to 1900*/
 period_1800_1900_novels(Book) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    novel(Book, Year), 
+    (Year >= 1800, Year =< 1900).
 
 /* Characters who are fans of LOTR */
 lotr_fans(Fan) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    fan(Fan, BookList), 
+    member(the_lord_of_the_rings, BookList).
 
 /* Authors of the novels owned by Chandler */
 author_names(Author) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    fan(chandler, BookList), 
+    member(OneBook, BookList), 
+    author(Author, ABooks), 
+    member(OneBook, ABooks).
 
 /* Characters who are fans of Brandon Sanderson's novels */
 fans_names(Fan) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    fan(Fan, BookList), 
+    author(brandon_sanderson, ABooks), 
+    member(OneBook, BookList), 
+    member(OneBook, ABooks).
 
 /* Novels common between either of Phoebe, Ross, and Monica */
 mutual_novels(Book) :-
-    %% remove fail and add body/other cases for this predicate
-    fail.
+    fan(phoebe, PBooks),
+    fan(ross, RBooks),
+    fan(monica, MBooks), 
+    (member(Book, PBooks), member(Book, RBooks);
+     member(Book, PBooks), member(Book, MBooks);
+     member(Book, RBooks), member(Book, MBooks)).
